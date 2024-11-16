fd259822264c4445029ff1e46b9df4814cec8775
LobnaEla <lobna.elabed@gmail.com>
2024-11-17 00:35:30 +0100

    WIP: Save local changes before pulling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './componenets/home/home';
 import About from './componenets/about';
+import Apartments from './componenets/categories/apartments';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path='' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/apartments' element={<Apartments />} />
       </Routes>
     </div>
   );
